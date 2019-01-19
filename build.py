73800f6aceb177d7b8efff80e26854301f4d0e79
Jerry W <jerrywiltse@gmail.com>
2019-01-19 13:20:33 -0500

    try adding dll with static runtime build

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder()
+    builder = build_template_default.get_builder(dll_with_static_runtime=True)
     
     builder.remove_build_if(lambda build: build.options["winpcap:shared"] == False)
 
